7bec9126e5480c75f947eb3446f2adb5c67284ac
matsue-code <mbolamathew@icloud.com>
2025-09-01 18:47:10 +0300

    third commit

---
+++ main3.py
@@ -0,0 +1,8 @@
+country_code = {'india': '0091',
+                'kenya': '254',
+                'nepal':'0097'       
+}
+print("country code for india-")
+print(country_code.get('india','not found'))
+print("country code for japan-")
+print(country_code.get('japan','not found'))
